dbdf36443ba496cb3a68909987807e63f41df85f
Slamy <palmchen1000@web.de>
2020-11-26 22:28:46 +0100

    works with PSn00bSDK

--- toolchain/buildToolchain.sh
+++ toolchain/buildToolchain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TARGETARCHITECTURE=mipsel-elf
+TARGETARCHITECTURE=mipsel-unknown-elf
 
 # Configure your path
-HOSTINSTALLPATH="/opt/mipsel-elf.slamy"
+HOSTINSTALLPATH="/opt/mipsel-unknown-elf.slamy"
 
 # Configure as much as you like
 MAKE_JOBS=${nproc}
@@ -30,23 +30,20 @@ function fetchAndExtract()
 
 function fetch_archives ()
 {
-	fetchAndExtract https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz binutils-2.32
-	fetchAndExtract ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-9.1.0/gcc-9.1.0.tar.gz gcc-9.1.0
-	fetchAndExtract ftp://sourceware.org/pub/newlib/newlib-3.1.0.tar.gz newlib-3.1.0
+	fetchAndExtract https://ftp.gnu.org/gnu/binutils/binutils-2.31.tar.gz binutils-2.31
+	fetchAndExtract ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-7.4.0/gcc-7.4.0.tar.gz gcc-7.4.0
 }
 
 function build_binutils () {
 	echo ---------------------- Building binutils -------------------------
 	
-	cd binutils-2.32
+	cd binutils-2.31
 	mkdir -p build
 	cd build
 	
-
 	../configure \
 		--target=$TARGETARCHITECTURE \
 		--prefix=$HOSTINSTALLPATH/ \
-		--with-headers=../../newlib-3.1.0/newlib/libc/include/ \
 		--with-float=soft
 	
 	make -j$MAKE_JOBS
@@ -54,76 +51,27 @@ function build_binutils () {
 	cd ../..
 }
 
-# --disable-bootstrap					Dont test the GCC as the Target is not the Host
-# --disable-newlib-io-float				Disable printf/scanf family float support.
-# --enable-newlib-nano-formatted-io		This builds NEWLIB with a special implementation of formatted I/O functions, 
-#										Designed to lower the size of application on small systems
-# --disable-multilib						Do NOT generate for every library version. Only 68000 is needed here
-# --enable-cxx-flags='...'				Custom flags for compiling libstdc++. In this case optimizing for size.	
-# --disable-libssp						Disable stack smash protection
-# --disable-libgomp						GNU Offloading and Multi Processing Runtime Library not needed
-
-
-function build_bootstrap_gcc () {
+function build_final_gcc () {
 	echo ---------------------- Building bootstrap GCC -------------------------
 	
-	cd gcc-9.1.0/
+	cd gcc-7.4.0/
 	mkdir -p build
 	cd build
 	
 	../configure \
 		--target=$TARGETARCHITECTURE \
 		--prefix=$HOSTINSTALLPATH \
-		--enable-languages=c,c++ \
-		--disable-bootstrap \
-		--with-newlib \
-		--disable-libmudflap \
-		--enable-lto \
-		--disable-libssp \
-		--disable-libgomp \
-		--disable-threads \
-		--with-gnu-as \
-		--with-gnu-ld \
-		--with-float=soft \
 		--disable-nls \
-		--disable-libstdcxx-threads \
-		--disable-multilib \
-		--with-headers=../../newlib-3.1.0/newlib/libc/include/ \
-		--enable-cxx-flags='-fomit-frame-pointer -ffunction-sections -fno-exceptions'
+		--disable-libada \
+		--disable-libssp \
+		--disable-libquadmath \
+		--disable-libstdc++-v3 \
+		--with-float=soft \
+		--enable-languages=c,c++ \
+		--with-gnu-as \
+		--with-gnu-ld
 		
 	#build only gcc
-	make -j$MAKE_JOBS all-gcc
-	make install-gcc
-	cd ../..
-}
-
-function build_newlib () {
-	echo ---------------------- Building Newlib -------------------------
-	
-	cd  newlib-3.1.0/
-	mkdir -p build
-	cd build
-	
-	../configure \
-		--target=$TARGETARCHITECTURE \
-		--prefix=$HOSTINSTALLPATH \
-		--disable-multilib \
-		--enable-newlib-nano-formatted-io \
-		--disable-newlib-io-float \
-		--with-float=soft \
-		--disable-newlib-supplied-syscalls \
-		--enable-languages=c,c++ \
-		--with-headers=../../newlib-3.1.0/newlib/libc/include/
-		
-	make -j$MAKE_JOBS
-	make install
-	cd ../..
-}
-
-function build_final_gcc () {
-	echo ---------------------- Building Rest of GCC and Libstdc++ -------------------------
-	
-	cd gcc-9.1.0/build
 	make -j$MAKE_JOBS
 	make install
 	cd ../..
@@ -132,7 +80,5 @@ function build_final_gcc () {
 
 fetch_archives
 build_binutils
-build_bootstrap_gcc
-build_newlib
 build_final_gcc
 
